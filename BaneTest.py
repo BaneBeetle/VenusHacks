fa25c9b3c58a2a72fa40107748781dcbcefaa101
BaneBeetle <lollypopbjsoldierlollypop@gmail.com>
2024-05-24 21:06:58 -0700

    Added input function for testing. Prompt asks for a school subject and will help create prompt for GPT to provide relevent videos.

--- BaneTest.py
+++ BaneTest.py
@@ -4,11 +4,14 @@ openai.api_key = OPENAI_API_KEY
 
 client = openai.OpenAI(api_key=OPENAI_API_KEY)
 
+subject = input("Please provide a subject!\n")
+prompt = f"Please provide video links for {subject}"
 stream = client.chat.completions.create(
     model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": "Give me videos for computer science on youtube, only provide links"}],
+    messages=[{"role": "user", "content": prompt}],
     stream=True,
 )
 for chunk in stream:
     if chunk.choices[0].delta.content is not None:
         print(chunk.choices[0].delta.content, end="")
+
