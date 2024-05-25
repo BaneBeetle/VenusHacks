3d1b780b3dd76bf6402533cfe6d3d5f83716d12d
leahkangg <leahkang0831@gmail.com>
2024-05-24 23:02:12 -0700

    leahs flask and html/css for cafes

---
+++ BACKEND/webflask.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+
+YELP_API_KEY = 'hYkh7Y38ofnKkkZCLXNoSzo9btQj7eYM7v0hbAyQ0gWSPvm236SOY1RB9oaPg5x1OA8dozm2tICepSMNZpd4XLXUiXyu2HTUa8dOEOJdQ8C8wNeRd1e-cvh35mxRZnYx'
+YELP_API_URL = 'https://api.yelp.com/v3/businesses/search'
+
+
+# PYTHON FUNCTIONS
+
+
+def search_cafes_in_city(city):
+    headers = {'Authorization': f'Bearer {YELP_API_KEY}'}
+    params = {'term': 'cafe', 'location': city}
+    response = requests.get(YELP_API_URL, headers=headers, params=params)
+    
+    if response.status_code == 200:
+        cafes = response.json()['businesses']
+        return [cafe['name'] for cafe in cafes]  
+    else:
+        return None
+
+
+#FLASK AREA
+
+@app.route('/', methods=['GET', 'POST'])
+
+def index():
+    cafes = None
+    if request.method == 'POST':
+        city_name = request.form['city']
+        cafes = search_cafes_in_city(city_name)
+        return render_template('caferesultpage.html', cafes=cafes)
+    return render_template('cafes.html', cafes=cafes)
+
+if __name__ == '__main__':
+    app.run(debug=True)
